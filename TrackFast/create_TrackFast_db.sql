328e0162a8c14d81235c2bd082743ad1aa135002
Nathan Rodrigues Vieira <nathanrodriguesvieira99@gmail.com>
2025-11-13 18:20:46 -0300

    style: format create_TrackFast_db.sql file

--- TrackFast/create_TrackFast_db.sql
+++ TrackFast/create_TrackFast_db.sql
@@ -2,24 +2,27 @@ DROP TABLE IF EXISTS entrega;
 DROP TABLE IF EXISTS cliente;
 DROP TABLE IF EXISTS motorista;
 
-CREATE TABLE motorista (
-    id SERIAL PRIMARY KEY,
-    nome TEXT NOT NULL,
+CREATE TABLE motorista
+(
+    id     SERIAL PRIMARY KEY,
+    nome   TEXT NOT NULL,
     regiao TEXT NOT NULL
 );
 
-CREATE  TABLE cliente  (
-    id SERIAL PRIMARY KEY,
-    nome TEXT NOT NULL,
+CREATE TABLE cliente
+(
+    id     SERIAL PRIMARY KEY,
+    nome   TEXT NOT NULL,
     cidade TEXT NOT NULL
 );
 
-CREATE TABLE entrega (
-    id SERIAL PRIMARY KEY,
-    motorista_id INT NOT NULL REFERENCES motorista(id),
-    cidade TEXT NOT NULL,
-    data_envio DATE NOT NULL,
+CREATE TABLE entrega
+(
+    id           SERIAL PRIMARY KEY,
+    motorista_id INT            NOT NULL REFERENCES motorista (id),
+    cidade       TEXT           NOT NULL,
+    data_envio   DATE           NOT NULL,
     data_entrega DATE,
-    status TEXT NOT NULL,
-    valor NUMERIC(10,2) NOT NULL
+    status       TEXT           NOT NULL,
+    valor        NUMERIC(10, 2) NOT NULL
 );
