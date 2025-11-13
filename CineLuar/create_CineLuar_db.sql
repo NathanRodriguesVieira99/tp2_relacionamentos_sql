367537acd048978958d3ccaef3b99202607fc941
Nathan Rodrigues Vieira <nathanrodriguesvieira99@gmail.com>
2025-11-13 18:18:12 -0300

    style: format create_CineLuar_db.sql file

--- CineLuar/create_CineLuar_db.sql
+++ CineLuar/create_CineLuar_db.sql
@@ -2,24 +2,27 @@ DROP TABLE IF EXISTS filme;
 DROP TABLE IF EXISTS jurado;
 DROP TABLE IF EXISTS avaliacao;
 
-CREATE TABLE filme (
-    id SERIAL PRIMARY KEY,
-    titulo TEXT NOT NULL,
-    diretor TEXT  NOT NULL,
-    pais TEXT NOT NULL,
-    ano TEXT NOT NULL
+CREATE TABLE filme
+(
+    id      SERIAL PRIMARY KEY,
+    titulo  TEXT NOT NULL,
+    diretor TEXT NOT NULL,
+    pais    TEXT NOT NULL,
+    ano     TEXT NOT NULL
 );
 
-CREATE TABLE jurado (
-    id SERIAL PRIMARY KEY,
+CREATE TABLE jurado
+(
+    id   SERIAL PRIMARY KEY,
     nome TEXT NOT NULL,
     pais TEXT NOT NULL
 );
 
-CREATE TABLE avaliacao (
-  id SERIAL PRIMARY KEY,
-  filme_id INT NOT NULL REFERENCES filme(id),
-  jurado_id INT NOT NULL REFERENCES jurado(id),
-  nota NUMERIC(3,1) NOT NULL,
-  comentario TEXT NOT NULL
+CREATE TABLE avaliacao
+(
+    id         SERIAL PRIMARY KEY,
+    filme_id   INT           NOT NULL REFERENCES filme (id),
+    jurado_id  INT           NOT NULL REFERENCES jurado (id),
+    nota       NUMERIC(3, 1) NOT NULL,
+    comentario TEXT          NOT NULL
 );
